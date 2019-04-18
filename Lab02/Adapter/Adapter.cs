ba74b201d481d1c131eaa27f2fb5fe496643e170
duongphilong <49159260+duuongphilong@users.noreply.github.com>
2019-04-18 21:21:03 +0700

    new project

---
+++ Lab02/Adapter/Adapter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+	class Adapter: Target
+	{
+		private Adaptee _adaptee = new Adaptee();
+		public override void Request()
+		{
+			// Possibly do some other work
+			// and then call SpecificRequest
+			_adaptee.SpecificRequest();
+		}
+	}
+	/// <summary>
+	/// The 'Adaptee' class
+	/// </summary>
+	class Adaptee
+	{
+		public void SpecificRequest()
+		{
+			Console.WriteLine("Called SpecificRequest()");
+		}
+	}
+}
---
+++ Lab02/Decorator_Real-World/LibraryItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator_Real_World
+{
+	abstract class LibraryItem
+	{
+		private int _numCopies;
+		// Property
+		public int NumCopies
+		{
+			get { return _numCopies; }
+			set { _numCopies = value; }
+		}
+		public abstract void Display();
+	}
+}
